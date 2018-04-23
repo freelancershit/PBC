ce241e0a38706d9fcf903a17cdc8fe9a79a3c1c8
Jose Carlo R. San Pedro <carlo.sanpedro@ubidy.com>
2018-04-23 20:04:02 +0800

    move address and contact in footer

--- routes/user.js
+++ routes/user.js
@@ -1,119 +1,121 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var User = require("../models/user");
-var async = require("async");
-var passport = require("passport");
-var passportConfig = require("../config/passport");
+var User = require('../models/user');
+var async = require('async');
+var passport = require('passport');
+var passportConfig = require('../config/passport');
 
-
-router.get("/login", function(req, res){
-	if(req.user) return res.redirect("/");
-	res.render("accounts/login", {message: req.flash("loginMessage")});
+router.get('/login', function(req, res) {
+  if (req.user) return res.redirect('/');
+  res.render('accounts/login', { message: req.flash('loginMessage') });
 });
 
-router.post("/login", passport.authenticate("local-login", {
-	successRedirect: "/",
-	failureRedirect: "/login",
-	failureFlash: true
-}), function(req, res){
+router.post(
+  '/login',
+  passport.authenticate('local-login', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true,
+  }),
+  function(req, res) {},
+);
 
-});
-
-router.get("/profile", function(req, res){
-	// User
-	// .findOne({_id: req.user._id})
-	// .populate('history.item')
-	// .exec(function(err, user){
-	// 	if(err) return next(err);
-	// 	res.render('accounts/profile', {user : user});
-	// });
-	async.waterfall([
-		function(callback){
-			User
-			.findById({_id: req.user._id})
-			.populate('history')
-			.exec(function(err, user){
-				if(err) return next(err);
-				console.log(user);
-				callback(null, user);
-			});
-		}, function(user){
-			History
-			.find({customer: req.user.email})
-			.populate('item')
-			.exec(function(err, history){
-				if(err){
-					console.log(err);
-				} 
-				res.render('accounts/profile', {user: user, history: history});
-			});
-		}
-		]);
-});
-
-router.get("/signup", function(req, res){
-	res.render("accounts/signup", {errors: req.flash("errors")});
-});
-
-router.post("/signup", function(req, res, next){
-
-	// async.waterfall([
-		// function(callback){
-		var user = new User();
-		user.profile.name = req.body.firstName + " " + req.body.middleName + " " + req.body.lastName;
-        user.email = req.body.email;
-        if(user.isAdmin){
-            User.count({name: regex}).exec(function(err, count){
-            var number = (new Date()).getFullYear() +  "000000";
-            user.idNumber = parseInt(number) + count;
+router.get('/profile', function(req, res) {
+  // User
+  // .findOne({_id: req.user._id})
+  // .populate('history.item')
+  // .exec(function(err, user){
+  // 	if(err) return next(err);
+  // 	res.render('accounts/profile', {user : user});
+  // });
+  async.waterfall([
+    function(callback) {
+      User.findById({ _id: req.user._id })
+        .populate('history')
+        .exec(function(err, user) {
+          if (err) return next(err);
+          console.log(user);
+          callback(null, user);
         });
-        }
-		user.profile.picture = user.gravatar();
-		user.birthdate = new Date(req.body.year + "-" + req.body.month + "-" + req.body.day);
-		if(!(req.body.password === req.body.confirmPassword)){
-			req.flash("errors", "Your password and confirm password does not match");
-			return res.redirect("/signup");
-		}
-		user.password = req.body.password;
-		User.findOne({email : req.body.email}, function(err, existingUser){
-		if(existingUser){
-			req.flash("errors", "Account with that email address already exist");
-			return res.redirect("/signup");
-		} else{
-			user.save(function(err, user){
-                if(err) return next(err);
-                req.logIn(user, function(err){
-                    res.redirect("/admininterface");
-                })
-            });
-		}
-	});
-	// 	},
-	// 	function(user){
-	// 		var cart = new Cart();
-	// 		cart.owner = user._id;
-	// 		cart.save(function(err){
-	// 			if(err) return next(err);
-	// 			req.logIn(user, function(err){
-	// 			return res.redirect("/");
-	// 			});
-	// 		});
-	// 	}
-	// ]);
+    },
+    function(user) {
+      History.find({ customer: req.user.email })
+        .populate('item')
+        .exec(function(err, history) {
+          if (err) {
+            console.log(err);
+          }
+          res.render('accounts/profile', { user: user, history: history });
+        });
+    },
+  ]);
 });
 
-router.get("/logout", function(req, res, next){
-	User.findById(req.user._id, function(err, user){
-		user.save(function(err, result){
-			if(err) return next(err);
-			console.log(result);
-		});
-	});
-	
+router.get('/signup', function(req, res) {
+  res.render('accounts/signup', { errors: req.flash('errors') });
+});
 
-	req.logout();
-	res.redirect("/");
+router.post('/signup', function(req, res, next) {
+  // async.waterfall([
+  // function(callback){
+  var user = new User();
+  user.superUser = true;
+  user.isAdmin = true;
+  user.profile.name =
+    req.body.firstName + ' ' + req.body.middleName + ' ' + req.body.lastName;
+  user.email = req.body.email;
+  if (user.isAdmin) {
+    User.count({ name: regex }).exec(function(err, count) {
+      var number = new Date().getFullYear() + '000000';
+      user.idNumber = parseInt(number) + count;
+    });
+  }
+  user.profile.picture = user.gravatar();
+  user.birthdate = new Date(
+    req.body.year + '-' + req.body.month + '-' + req.body.day,
+  );
+  if (!(req.body.password === req.body.confirmPassword)) {
+    req.flash('errors', 'Your password and confirm password does not match');
+    return res.redirect('/signup');
+  }
+  user.password = req.body.password;
+  User.findOne({ email: req.body.email }, function(err, existingUser) {
+    if (existingUser) {
+      req.flash('errors', 'Account with that email address already exist');
+      return res.redirect('/signup');
+    } else {
+      user.save(function(err, user) {
+        if (err) return next(err);
+        req.logIn(user, function(err) {
+          res.redirect('/admininterface');
+        });
+      });
+    }
+  });
+  // 	},
+  // 	function(user){
+  // 		var cart = new Cart();
+  // 		cart.owner = user._id;
+  // 		cart.save(function(err){
+  // 			if(err) return next(err);
+  // 			req.logIn(user, function(err){
+  // 			return res.redirect("/");
+  // 			});
+  // 		});
+  // 	}
+  // ]);
+});
 
+router.get('/logout', function(req, res, next) {
+  User.findById(req.user._id, function(err, user) {
+    user.save(function(err, result) {
+      if (err) return next(err);
+      console.log(result);
+    });
+  });
+
+  req.logout();
+  res.redirect('/');
 });
 
 // router.get("/edit-profile", function(req, res, next){
@@ -152,11 +154,9 @@ router.get("/logout", function(req, res, next){
 // 		user.save(function(err){
 // 			if(err) return next(err);
 // 			req.flash("success", "You successfully edit your profile");
-// 			return res.redirect("/edit-profile");	
+// 			return res.redirect("/edit-profile");
 // 		});
 // 	});
 // });
-
-
 
 module.exports = router;
