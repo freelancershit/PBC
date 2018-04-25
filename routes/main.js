1f7126b80c0569e7d9603f256149ca644a476f92
Jose Carlo R. San Pedro <carlo.sanpedro@ubidy.com>
2018-04-25 13:03:53 +0800

    changes: add admin view student grade list and view per student

--- routes/main.js
+++ routes/main.js
@@ -23,6 +23,13 @@ router.get('/managenewsandannouncements', function(req, res, next) {
   res.render('admin/managenews');
 });
 
+router.get('/studentlist', function(req, res, next) {
+  res.render('admin/studentlist');
+});
+
+router.get('/adminviewgrade', function(req, res, next) {
+  res.render('admin/viewgrade');
+});
 
 router.get('/postnewsandannouncements', function(req, res, next) {
   res.render('admin/postnews');
