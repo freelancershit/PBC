a5bb38796c8b0de37099fcc274881684c47f5d55
nicho <nguimbaolibot.chrs@gmail.com>
2018-04-29 23:30:57 +0800

    faculty functions

--- routes/faculty.js
+++ routes/faculty.js
@@ -13,7 +13,14 @@ var passportConfig = require("../config/passport");
 router.get("/encode", function(req, res, next){
     Subject.find({faculty: req.user._id}, function(err, subjects){
         if(err) return next(err);
-        res.render("faculty/list", {subjects: subjects});
+        res.render("faculty/viewencoded-grades", {subjects: subjects});
+    });
+});
+
+router.get("/encode/:yr/:sec/:subj", function(req, res, next){
+    Subject.find({yrLvl: req.params.yr, section: req.params.sec, subject: req.params.subject}, function(err, subject){
+        if(err) return next(err);
+        res.render("faculty/viewgrades", {subject : subject});
     });
 });
 
