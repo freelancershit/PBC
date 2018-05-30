d74dfa1e50c801f203041c7a2cb2185023fa34ac
Jose Carlo <carlo.sanpedro@ubidy.com>
2018-05-31 00:00:25 +0800

    fixed

--- routes/main.js
+++ routes/main.js
@@ -11,46 +11,66 @@ var Literary = require('../models/literary');
 
 router.use(expressSanitizer());
 
-router.get('/search', function(req, res, next) {
+router.get('/search', function (req, res, next) {
   if (req.query.search == 'news') {
     const regex = new RegExp(escapeRegex(req.query.search), 'gi');
-    News.find({ category: regex, archive: false }, function(err, allNews) {
+    News.find({
+      category: regex,
+      archive: false
+    }, function (err, allNews) {
       if (err) return next(err);
-      return res.render('main/search', { allNews: allNews });
+      return res.render('main/search', {
+        allNews: allNews
+      });
     });
   } else if (
     req.query.search == 'announcement' ||
     req.query.search == 'announcements'
   ) {
     const regex = new RegExp(escapeRegex(req.query.search), 'gi');
-    News.find({ category: regex, archive: false }, function(err, allNews) {
+    News.find({
+      category: regex,
+      archive: false
+    }, function (err, allNews) {
       if (err) return next(err);
-      return res.render('main/search', { allNews: allNews });
+      return res.render('main/search', {
+        allNews: allNews
+      });
     });
   } else if (req.query.search) {
     console.log(req.query.search);
     const regex = new RegExp(escapeRegex(req.query.search), 'gi');
-    News.find(
-      {
-        $or: [
-          { title: regex, archive: false },
-          { content: regex, archive: false },
+    News.find({
+        $or: [{
+            title: regex,
+            archive: false
+          },
+          {
+            content: regex,
+            archive: false
+          },
         ],
       },
-      function(err, allNews) {
+      function (err, allNews) {
         if (err) return next(err);
-        return res.render('main/search', { allNews: allNews });
+        return res.render('main/search', {
+          allNews: allNews,
+        });
       },
     );
   } else {
-    News.find({ archive: false }, function(err, allNews) {
+    News.find({
+      archive: false
+    }, function (err, allNews) {
       if (err) return next(err);
-      return res.render('main/search', { allNews: allNews });
+      return res.render('main/search', {
+        allNews: allNews
+      });
     });
   }
 });
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   if (req.user) {
     if (req.user.deactivated) {
       req.logout();
@@ -58,12 +78,22 @@ router.get('/', function(req, res, next) {
       return res.redirect('/');
     }
   }
-  News.findOne({ category: 'news', archive: false })
-    .sort({ postNumber: -1 })
-    .exec(function(err1, news) {
-      News.findOne({ category: 'announcement', archive: false })
-        .sort({ postNumber: -1 })
-        .exec(function(err, announcements) {
+  News.findOne({
+      category: 'news',
+      archive: false
+    })
+    .sort({
+      postNumber: -1
+    })
+    .exec(function (err1, news) {
+      News.findOne({
+          category: 'announcement',
+          archive: false
+        })
+        .sort({
+          postNumber: -1
+        })
+        .exec(function (err, announcements) {
           if (err) return next(err1);
           if (err) return next(err);
 
@@ -75,116 +105,128 @@ router.get('/', function(req, res, next) {
     });
 });
 
-router.get('/activities', function(req, res, next) {
+router.get('/activities', function (req, res, next) {
   res.render('main/activities');
 });
 
-router.get('/search', function(req, res, next) {
+router.get('/search', function (req, res, next) {
   res.render('main/search');
 });
 
-router.get('/admindashboard', function(req, res, next) {
+router.get('/admindashboard', function (req, res, next) {
   res.render('admin/dashboard');
 });
 
-router.get('/registrationlist', function(req, res, next) {
+router.get('/registrationlist', function (req, res, next) {
   res.render('admin/registrationlist');
 });
 
-router.get('/abc', function(req, res, next) {
-  Subject.find({ faculty: '5ae5def3e4708d7dde8a190e' }, function(
+router.get('/abc', function (req, res, next) {
+  Subject.find({
+    faculty: '5ae5def3e4708d7dde8a190e'
+  }, function (
     err,
     subjects,
   ) {
     if (err) return next(err);
     console.log(subjects);
-    res.render('main/abc', { subjects: subjects });
+    res.render('main/abc', {
+      subjects: subjects
+    });
   });
 });
 
-router.post('/abc', function(req, res, next) {
+router.post('/abc', function (req, res, next) {
   console.log(req.body.abc);
 });
-router.post('/abc1', function(req, res, next) {
+router.post('/abc1', function (req, res, next) {
   console.log(req.body.abc);
 });
 
-router.get('/adminviewgrade', function(req, res, next) {
+router.get('/adminviewgrade', function (req, res, next) {
   res.render('admin/viewgrade');
 });
 
-router.get('/facultydashboard', function(req, res, next) {
+router.get('/facultydashboard', function (req, res, next) {
   res.render('faculty/dashboard');
 });
-router.get('/facultsetting', function(req, res, next) {
+router.get('/facultsetting', function (req, res, next) {
   res.render('faculty/accountsetting');
 });
 
-router.get('/studentdashboard', function(req, res, next) {
+router.get('/studentdashboard', function (req, res, next) {
   res.render('student/dashboard');
 });
 
-router.get('/viewgrade', function(req, res, next) {
+router.get('/viewgrade', function (req, res, next) {
   res.render('student/grade');
 });
 
-router.get('/admissionreq', function(req, res, next) {
+router.get('/admissionreq', function (req, res, next) {
   res.render('main/admissionreq');
 });
 
-router.get('/awards', function(req, res, next) {
+router.get('/awards', function (req, res, next) {
   res.render('main/awards');
 });
 
-router.get('/clubs', function(req, res, next) {
+router.get('/clubs', function (req, res, next) {
   res.render('main/clubs');
 });
 
-router.get('/curriculum', function(req, res, next) {
+router.get('/curriculum', function (req, res, next) {
   res.render('main/curriculum');
 });
 
-router.get('/facilities', function(req, res, next) {
+router.get('/facilities', function (req, res, next) {
   res.render('main/facilities');
 });
-router.get('/viewpubs', function(req, res, next) {
+router.get('/viewpubs', function (req, res, next) {
   res.render('admin/viewpublication');
 });
-router.get('/faculty', function(req, res, next) {
+router.get('/faculty', function (req, res, next) {
   res.render('main/faculty');
 });
-router.get('/publication', function(req, res, next) {
-  Literary.find({ status: true }, function(err, literaries) {
-    res.render('main/Publication', { literaries: literaries });
+router.get('/publication', function (req, res, next) {
+  Literary.find({
+    status: true
+  }, function (err, literaries) {
+    res.render('main/Publication', {
+      literaries: literaries
+    });
   });
 });
-router.get('/facultyviewpubs', function(req, res, next) {
+router.get('/facultyviewpubs', function (req, res, next) {
   res.render('faculty/viewpublication');
 });
-router.get('/adminsetting', function(req, res, next) {
+router.get('/adminsetting', function (req, res, next) {
   res.render('admin/accountsetting');
 });
-router.get('/studentsetting', function(req, res, next) {
+router.get('/studentsetting', function (req, res, next) {
   res.render('student/accountsetting');
 });
 
-router.get('/facultymanagepubs', function(req, res, next) {
-  Literary.find({}, function(err, allPending) {
+router.get('/facultymanagepubs', function (req, res, next) {
+  Literary.find({}, function (err, allPending) {
     if (err) return next(err);
-    res.render('faculty/managepublication', { allPending: allPending });
+    res.render('faculty/managepublication', {
+      allPending: allPending
+    });
   });
 });
 
-router.get('/register', function(req, res, next) {
+router.get('/register', function (req, res, next) {
   res.render('main/register', {
     errors: req.flash('errors'),
     success: req.flash('success'),
   });
 });
 
-router.post('/register', function(req, res, next) {
+router.post('/register', function (req, res, next) {
   var pending = new Pending();
-  User.findOne({ email: req.body.email }, function(err, existingUser) {
+  User.findOne({
+    email: req.body.email
+  }, function (err, existingUser) {
     if (existingUser) {
       req.flash('errors', 'Account with that email address already exist');
       return res.redirect('back');
@@ -207,7 +249,7 @@ router.post('/register', function(req, res, next) {
         pending.gender = req.body.gender;
         pending.email = req.body.email;
         pending.contact = req.body.contact;
-        pending.save(function(err, pendingUser) {
+        pending.save(function (err, pendingUser) {
           console.log(req.body.gender);
           if (err) return next(err);
           req.flash(
@@ -224,49 +266,56 @@ router.post('/register', function(req, res, next) {
   });
 });
 
-router.get('/gallery', function(req, res, next) {
+router.get('/gallery', function (req, res, next) {
   res.render('main/gallery');
 });
 
-router.get('/history', function(req, res, next) {
+router.get('/history', function (req, res, next) {
   res.render('main/history');
 });
 
-router.get('/home', function(req, res, next) {
+router.get('/home', function (req, res, next) {
   res.render('main/home');
 });
 
-router.get('/news', function(req, res, next) {
-  News.find({ category: 'news' }, function(err, news) {
+router.get('/news', function (req, res, next) {
+  News.find({
+    category: 'news'
+  }, function (err, news) {
     if (err) return next(err);
-    News.find({ category: 'announcement' }, function(err, announcements) {
+    News.find({
+      category: 'announcement'
+    }, function (err, announcements) {
       if (err) return next(err);
-      res.render('main/news', { news: news, announcements: announcements });
+      res.render('main/news', {
+        news: news,
+        announcements: announcements
+      });
     });
   });
 });
 
-router.get('/orgchart', function(req, res, next) {
+router.get('/orgchart', function (req, res, next) {
   res.render('main/orgchart');
 });
 
-router.get('/policies', function(req, res, next) {
+router.get('/policies', function (req, res, next) {
   res.render('main/policies');
 });
 
-router.get('/register', function(req, res, next) {
+router.get('/register', function (req, res, next) {
   res.render('main/register');
 });
 
-router.get('/scholarships', function(req, res, next) {
+router.get('/scholarships', function (req, res, next) {
   res.render('main/scholarships');
 });
 
-router.get('/service', function(req, res, next) {
+router.get('/service', function (req, res, next) {
   res.render('main/services');
 });
 
-router.get('/vmop', function(req, res, next) {
+router.get('/vmop', function (req, res, next) {
   res.render('main/vmop');
 });
 
